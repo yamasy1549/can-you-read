a408f5c07f6475b2393c7fe9cc04d5178fc9b749
yamasy1549 <sanae@yamasy.info>
2017-02-23 00:17:32 +0900

    画像表示

--- src/components/Answer.js
+++ src/components/Answer.js
@@ -2,13 +2,21 @@ import React, { PropTypes } from 'react'
 import { connect }          from 'react-redux'
 import styles               from './Answer.css'
 
-const Answer = ({ kana, description, correct, onNextClick }) => (
+const key = 'AIzaSyDDRFpo9sNZXwfRgmX4454_IJNubuuKFDQ'
+const size = '450x800'
+const scale = '2'
+const zoom = '8'
+const mapURL = `\/\/maps.google.com/maps/api/staticmap?key=${key}&size=${size}&scale=${scale}&zoom=${zoom}&markers=`
+console.log(mapURL)
+
+const Answer = ({ prefecture, kanji, kana, description, correct, onNextClick }) => (
   <div className={styles.answerWrapper}>
     <p>
       {correct ? '正解' : '不正解'}！
       {kana}
       <br />
       解説: {description}
+      <img src={mapURL + prefecture + kanji} alt={kanji} className={styles.map} />
       <button onClick={onNextClick}>
         次へ
       </button>
@@ -17,6 +25,8 @@ const Answer = ({ kana, description, correct, onNextClick }) => (
 )
 
 Answer.propTypes = {
+  prefecture:  PropTypes.string.isRequired,
+  kanji:       PropTypes.string.isRequired,
   kana:        PropTypes.string.isRequired,
   description: PropTypes.string.isRequired,
   onNextClick: PropTypes.func.isRequired
