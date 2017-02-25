d0ca35cab6826d32c8cff70d36741743f43d5dac
yamasy1549 <sanae@yamasy.info>
2017-02-25 13:57:52 +0900

    Answer部分のスタイルを当てた

--- src/components/Answer.js
+++ src/components/Answer.js
@@ -1,38 +1,54 @@
 import React, { PropTypes } from 'react'
 import { connect }          from 'react-redux'
+import { toJapanese }       from 'jp-num'
 import styles               from './Answer.css'
 
 const key = 'AIzaSyDDRFpo9sNZXwfRgmX4454_IJNubuuKFDQ'
 const size = '450x800'
-const scale = '2'
+const scale = '4'
 const zoom = '8'
 const mapURL = `\/\/maps.google.com/maps/api/staticmap?key=${key}&size=${size}&scale=${scale}&zoom=${zoom}&markers=`
 console.log(mapURL)
 
 const Answer = ({ prefecture, kanji, kana, description, quizCount, correctAnswer, currentQuiz, results, onNextClick, onResultClick }) => (
   <div className={styles.answerWrapper}>
-    <p>
-      {correctAnswer ? '正解' : '不正解'}！
-      {kana}
-      <br />
-      解説: {description}
+    <div className={styles.answer}>
+      <span className={styles.judgment}>
+        {correctAnswer ? '正解' : '不正解'}
+      </span>
+      <div className={styles.placeNameWrapper}>
+        <span className={styles.currentQuiz}>
+          第{toJapanese(String(currentQuiz))}問
+        </span>
+        <div className={styles.placeName}>
+          <span className={styles.kanji}>
+            {kanji}
+          </span>
+          <span className={styles.kana}>
+            {kana}
+          </span>
+        </div>
+        <p className={styles.description}>
+          {description}
+        </p>
+      </div>
       <img src={mapURL + prefecture + kanji} alt={kanji} className={styles.map} />
       {(() => {
         if(currentQuiz == quizCount) {
           return (
-            <button onClick={onResultClick}>
+            <button className={styles.nextButton} onClick={onResultClick}>
               結果
             </button>
           )
         } else {
           return (
-            <button onClick={onNextClick}>
+            <button className={styles.nextButton} onClick={onNextClick}>
               次へ
             </button>
           )
         }
       })()}
-    </p>
+    </div>
   </div>
 )
 
