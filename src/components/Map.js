673ecd759d2fea7b682216a601f3a9e321b78f77
yamasy1549 <sanae@yamasy.info>
2017-03-02 22:56:26 +0900

    [WIP] 地図の穴

--- src/components/Map.js
+++ src/components/Map.js
@@ -3,6 +3,8 @@ import styles            from './Map.css'
 import mapImg            from '../images/japanmap.png'
 
 const Map = () => (
-  <img src={mapImg} className={styles.map} alt='日本地図' id='animJapanMap' />
+  <div>
+    <img src={mapImg} className={styles.map} alt='日本地図' id='animJapanMap' />
+  </div>
 )
 export default Map
